306c0cf1efb01bff0a5254582c3e210c6272067d
ros <rrusso@ciaopeople.com>
2022-06-13 18:45:31 +0200

    Created all pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 
+const { resolve } = require('path')
+
 // vite.config.js
 export default defineConfig({
     build: {
-        cssCodeSplit: false
+        cssCodeSplit: false,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                sede: resolve(__dirname, 'scopri-la-sede/index.html'),
+                staff: resolve(__dirname, 'lo-staff-di-misture/index.html'),
+                eventi: resolve(__dirname, 'eventi-e-news/index.html'),
+                cocktail: resolve(__dirname, 'lista-dei-cocktail/index.html')
+            }
+        }
     }
 })
